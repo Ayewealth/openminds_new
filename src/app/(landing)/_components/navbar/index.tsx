6e8353dcc9dbe435e49a424c13c93b06f95dda56
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-10-17 01:19:58 +0100

    feat: initial commit

---
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import Menu from "./menu"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Logout } from "@/icons"
+import GlassSheet from "@/components/global/glass-sheet"
+import { MenuIcon } from "lucide-react"
+
+type Props = {}
+
+const LandingPageNavbar = (props: Props) => {
+    return (
+        <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
+            <p className="font-bold text-2xl text-white">Openminds.</p>
+            <Menu orientation="desktop" />
+            <div className="flex gap-2">
+                <Link href="/sign-in">
+                    <Button
+                        variant="outline"
+                        className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray hover:text-white text-white"
+                    >
+                        <Logout />
+                        Login
+                    </Button>
+                </Link>
+                <GlassSheet
+                    triggerClass="lg:hidden"
+                    trigger={
+                        <Button
+                            variant="ghost"
+                            className="hover:bg-transparent"
+                        >
+                            <MenuIcon size={25} stroke="#fff" />
+                        </Button>
+                    }
+                >
+                    <Menu orientation="mobile" />
+                </GlassSheet>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPageNavbar
---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+import { LANDING_PAGE_MENU, MenuProps } from "./menus"
+
+type OpenmindsConstantsProps = {
+    landingPageMenu: MenuProps[]
+    // signUpForm: AuthFormProps[]
+    // signInForm: AuthFormProps[]
+    // groupList: GroupListProps[]
+    // createGroupPlaceholder: CreateGroupPlaceholderProps[]
+    // groupPageMenu: GroupMenuProps[]
+}
+
+export const OPENMINDS_CONSTANTS: OpenmindsConstantsProps = {
+    landingPageMenu: LANDING_PAGE_MENU,
+    // signUpForm: SIGN_UP_FORM,
+    // signInForm: SIGN_IN_FORM,
+    // groupList: GROUP_LIST,
+    // createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
+    // groupPageMenu: GROUP_PAGE_MENU,
+}
